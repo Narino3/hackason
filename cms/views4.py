f5c4cfdb6e1881b80a051f4510da2a410c3a26c4
yutoe183 <sbcyutomh91997@i.softbank.jp>
2020-12-06 11:40:51 +0900

    zibun commit 4

--- cms/views4.py
+++ cms/views4.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render
 #from django.http import HttpResponse
 #from django.template import loader
+from django import forms
 from .models import Book, Recruit, Entry # DB
 
 #class ZibunView(TemplateView):
@@ -11,18 +12,39 @@ from .models import Book, Recruit, Entry # DB
 #class MemberList(ListView):
 #    model = Entry
 
+class EntryListForm(forms.ModelForm):
+    class Meta:
+        model = Entry
+        fields = ('entry_ID',)
+        labels = {
+            'entry_ID': 'マッチングID',
+        }
+
 def ZibunView(request):
-    data = Entry.objects.all()
-    params = {'page_type': 0, 'data': data}
+    #data = Entry.objects.all()
+    arg_recruit_ID = 1
+    recruit = Recruit.objects.get(recruit_ID=arg_recruit_ID)
+    entries = recruit.entry.all().order_by('entry_ID')
+    params = {'page_type': 0, 'recruit': recruit, 'entries': entries}
     return render(request, 'cms/zibun.html', params)
 
 def ZibunRegistration(request, arg_recruit_ID, arg_entry_ID):
+    arg_recruit_ID = request.POST('zibun_recruit_ID')
+    arg_entry_ID = request.POST('zibun_entry_ID')
     recruit = Recruit.objects.get(recruit_ID=arg_recruit_ID)
     recruit.finish_flag = True
     entry = Entry.objects.get(entry_ID=arg_entry_ID)
     entry.finish_flag = True
-    params = {'page_type': 1, 'data': entry}
+    params = {'page_type': 1, 'recruit': recruit}
     return render(request, 'cms/zibun.html', params)
+
+#def ZibunRegistration(request, arg_recruit_ID, arg_entry_ID):
+#    recruit = Recruit.objects.get(recruit_ID=arg_recruit_ID)
+#    recruit.finish_flag = True
+#    entry = Entry.objects.get(entry_ID=arg_entry_ID)
+#    entry.finish_flag = True
+#    params = {'page_type': 1, 'data': entry}
+#    return render(request, 'cms/zibun.html', params)
 
 #def zibun(request):
 #    template = loader.get_template('cms/zibun.html')
