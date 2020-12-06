0a09d6efc34585c9c5ae7c85e9954c9429c2cbd7
yutoe183 <sbcyutomh91997@i.softbank.jp>
2020-12-06 12:48:18 +0900

    zibun commit 5

--- cms/views4.py
+++ cms/views4.py
@@ -13,25 +13,34 @@ from .models import Book, Recruit, Entry # DB
 #    model = Entry
 
 def ZibunView(request):
-    #data = Entry.objects.all()
+    if request.method == 'POST':
+        arg_recruit_ID = request.POST['zibun_recruit_ID']
+        arg_entry_ID = request.POST['zibun_entry_ID']
+        arg_recruit = Recruit.objects.get(recruit_ID=arg_recruit_ID)
+        arg_recruit.finish_flag = True
+        arg_entry = Entry.objects.get(entry_ID=arg_entry_ID)
+        arg_entry.finish_flag = True
+
     arg_recruit_ID = 1
     recruit = Recruit.objects.get(recruit_ID=arg_recruit_ID)
     entries = Entry.objects.filter(recruit_ID=arg_recruit_ID)
     #entries = recruit.entry.all().order_by('entry_ID')
     matching_entries = entries.filter(finish_flag=True)
-    matching_ID = matching_entries[0].entry_ID
     params = {'page_type': 0, 'recruit': recruit, 'entries': entries, 'matching_entries': matching_entries}
     return render(request, 'cms/zibun.html', params)
 
-def post(request, arg_recruit_ID, arg_entry_ID):
-    arg_recruit_ID = request.POST('zibun_recruit_ID')
-    arg_entry_ID = request.POST('zibun_entry_ID')
-    recruit = Recruit.objects.get(recruit_ID=arg_recruit_ID)
-    recruit.finish_flag = True
-    entry = Entry.objects.get(entry_ID=arg_entry_ID)
-    entry.finish_flag = True
-    params = {'page_type': 1, 'recruit': recruit}
-    return render(request, 'cms/zibun.html', params)
+#def post(request):
+#    arg_recruit_ID = request.POST['zibun_recruit_ID']
+#    arg_entry_ID = request.POST['zibun_entry_ID']
+#    recruit = Recruit.objects.get(recruit_ID=arg_recruit_ID)
+#    recruit.finish_flag = True
+    #entries = Entry.objects.filter(recruit_ID=arg_recruit_ID)
+#    entries = recruit.entry.all().order_by('entry_ID')
+#    entry = Entry.objects.get(entry_ID=arg_entry_ID)
+#    entry.finish_flag = True
+#    matching_entries = entries.filter(finish_flag=True)
+#    params = {'page_type': 1, 'recruit': recruit, 'entries': entries, 'matching_entries': matching_entries}
+#    return render(request, 'cms/zibun.html', params)
 
 #def ZibunRegistration(request, arg_recruit_ID, arg_entry_ID):
 #    recruit = Recruit.objects.get(recruit_ID=arg_recruit_ID)
